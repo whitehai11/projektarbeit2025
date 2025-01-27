6c6e5f89401d792c8232eb304a48e76076d52038
NullPointerX <157622851+whitehai11@users.noreply.github.com>
2025-01-27 17:22:18 +0100

    server.js

--- static/server.js
+++ static/server.js
@@ -1,44 +1,37 @@
 const express = require('express');
-const { Gpio } = require('onoff'); // Bibliothek zum Steuern der GPIO-Pins
-const app = express();
+const { Gpio } = require('onoff'); 
 const port = 3000;
 
-// GPIO Pins definieren
-const ledPin = new Gpio(17, 'out'); // GPIO 17 für die LED
-const servoPin = new Gpio(18, 'out'); // GPIO 18 für den Servo
 
-// Middleware, um statische Dateien (HTML, CSS, JS) zu servieren
+const ledPin = new Gpio(17, 'out');
+const servoPin = new Gpio(18, 'out'); 
 app.use(express.static('public'));
 
-// Endpoint zum Ein- und Ausschalten der LED
 app.get('/led/:state', (req, res) => {
     const state = req.params.state;
     if (state === 'on') {
-        ledPin.writeSync(1);  // LED einschalten
+        ledPin.writeSync(1);  
         res.send('LED ist an');
     } else if (state === 'off') {
-        ledPin.writeSync(0);  // LED ausschalten
+        ledPin.writeSync(0);  
         res.send('LED ist aus');
     } else {
         res.send('Unbekannter LED-Status');
     }
 });
 
-// Endpoint zum Steuern des Servos
 app.get('/servo/:position', (req, res) => {
     const position = req.params.position;
     if (position === 'open') {
-        servoPin.writeSync(1);  // Servo öffnen
+        servoPin.writeSync(1);  
         res.send('Servo geöffnet');
     } else if (position === 'close') {
-        servoPin.writeSync(0);  // Servo schließen
+        servoPin.writeSync(0); 
         res.send('Servo geschlossen');
     } else {
         res.send('Unbekannte Servo-Position');
     }
 });
-
-// Server starten
 app.listen(port, () => {
     console.log(`Server läuft auf http://localhost:${port}`);
 });
